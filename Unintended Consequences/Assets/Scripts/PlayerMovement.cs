608ac8e606dc0dfe8a51b8d24348b15588022636
DivSam <48162524+DivSam@users.noreply.github.com>
2021-01-15 22:41:06 -0500

    Comments on script

--- Unintended Consequences/Assets/Scripts/PlayerMovement.cs
+++ Unintended Consequences/Assets/Scripts/PlayerMovement.cs
@@ -13,13 +13,22 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode rightKey;
 
     private Vector2 velocityVector;
+
+
+    //Movement velocity value
     public float movementVelocity = 1f;
 
-
+    //Camera object so we do not need to go search for the camera every time we need to use it
     private Camera theCam;
 
+
+    //Firing point where the bullet gets released
     public Transform firePoint;
+    //Bullet game object
     public GameObject bulletToFire;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Movement inputs (arrow keys)
         if (Input.GetKey(upKey))
         {
             velocityVector.y = movementVelocity;
@@ -56,17 +66,24 @@ public class PlayerMovement : MonoBehaviour
 
         myRigidBody.velocity = velocityVector;
 
-
+        //Mouse position input
         Vector3 mouse = Input.mousePosition;
+
 
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
 
+
+
+        //Distance between mouse and player object
         Vector2 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
 
+
+        //Angle between player object and mouse
         float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
 
+        //Mouse button input to fire a shot
         if (Input.GetMouseButtonDown(0))
         {
             Instantiate(bulletToFire, firePoint.position, transform.rotation);
