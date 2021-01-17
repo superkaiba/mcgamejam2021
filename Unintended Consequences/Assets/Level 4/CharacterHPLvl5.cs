5a5a4e209f3b929d5dc1b852d2b130cf05c719d4
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2021-01-17 13:16:54 -0500

    Fix bugs

--- Unintended Consequences/Assets/Level 4/CharacterHPLvl5.cs
+++ Unintended Consequences/Assets/Level 4/CharacterHPLvl5.cs
@@ -40,11 +40,11 @@ public class CharacterHPLvl5 : NetworkBehaviour
     }
 
 
-
+    [Command]
 
     public void onDamage(float damage)
     {
-        StartCoroutine(Flash());
+        StartFlashCoroutineOnClients();
         currentHealth -= damage;
         mySpriteRenderer.transform.localScale += scaleChange;
         if (currentHealth <= 0)
@@ -56,32 +56,48 @@ public class CharacterHPLvl5 : NetworkBehaviour
 
     void Die()
     {
+        if (isServer) CmdNextScene();
+        ClientDie();
         Destroy(gameObject);
-        CmdNextScene();
     }
-    [Command]
+
+    [ClientRpc]
+
+    void ClientDie()
+    {
+        Destroy(gameObject);
+    }
+
+    [Command(ignoreAuthority =true)]
     void CmdNextScene()
     {
         myNetworkManager.NextScene();
     }
 
+    [ClientRpc]
+    void StartFlashCoroutineOnClients()
+    {
+        StartCoroutine(Flash());
+    }
+
     IEnumerator Flash()
     {
         Debug.Log("hello");
-        if (currentHealth <= 20 && currentHealth>=16) { 
-        mySpriteRenderer.color = new Color(255f/255f, 160f/255f, 122f/255f);
+        if (currentHealth <= 20 && currentHealth >= 16)
+        {
+            mySpriteRenderer.color = new Color(255f / 255f, 160f / 255f, 122f / 255f);
         }
         else if (currentHealth < 16 && currentHealth >= 10)
         {
-            mySpriteRenderer.color = new Color(250f/255f, 128f/255f, 114f/255f);
+            mySpriteRenderer.color = new Color(250f / 255f, 128f / 255f, 114f / 255f);
         }
         else if (currentHealth < 10 && currentHealth >= 6)
         {
-            mySpriteRenderer.color = new Color(220f/255f, 20f/255f, 60f/255f);
+            mySpriteRenderer.color = new Color(220f / 255f, 20f / 255f, 60f / 255f);
         }
         else if (currentHealth < 6 && currentHealth >= 0)
         {
-            mySpriteRenderer.color = new Color(255f/255f, 0f/255f, 0f/255f);
+            mySpriteRenderer.color = new Color(255f / 255f, 0f / 255f, 0f / 255f);
         }
 
         yield return new WaitForSeconds(0.1f);
